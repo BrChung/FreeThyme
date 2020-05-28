06419dcce169f03e4775e3c87dafab25fd35720d
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-05-28 15:38:20 -0700

    Header

--- freethyme/src/app/shared/shared.module.ts
+++ freethyme/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 import { NbCardModule, NbButtonModule } from "@nebular/theme";
+import { HeaderComponent } from "./header/header.component";
 
-const components = [];
+const components = [HeaderComponent];
 
-const modules = [CommonModule, NbCardModule, NbButtonModule];
+const modules = [CommonModule, NbCardModule, NbButtonModule, RouterModule];
 
 @NgModule({
   declarations: [...components],
