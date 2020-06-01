35b828fcba603ef9f596f9bcc75b3896f90190af
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-05-31 17:03:58 -0700

    Material Ripple

--- freethyme/src/app/shared/shared.module.ts
+++ freethyme/src/app/shared/shared.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { NbCardModule, NbButtonModule, NbIconModule } from "@nebular/theme";
-
 import { NbEvaIconsModule } from "@nebular/eva-icons";
 import { EllipsisModule } from "ngx-ellipsis";
+import { MatRippleModule } from "@angular/material/core";
 import { HeaderComponent } from "./header/header.component";
 
 const components = [HeaderComponent];
@@ -17,6 +17,7 @@ const modules = [
   NbIconModule,
   NbEvaIconsModule,
   EllipsisModule,
+  MatRippleModule,
 ];
 
 @NgModule({
