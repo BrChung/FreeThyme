b85cb07c15ed3ca0278c594817c87be6a3882eee
Jason Jose <jasonsisonjose@csu.fullerton.edu>
2020-06-08 19:09:06 -0700

    fixed typos

--- functions/calculate-free-time/main.py
+++ functions/calculate-free-time/main.py
@@ -33,7 +33,7 @@ def calculate_free_time(data, context):
     @firestore.transactional
     def find_free_time(transaction, group_cal_ref):
         freetime_list = list()
-        group_cal_snapshot = group_cal_ref.get(transaction = transction)
+        group_cal_snapshot = group_cal_ref.get(transaction = transaction)
 
         if group_cal_snapshot.exists:
             # freetime start = the end of the last event
@@ -42,22 +42,22 @@ def calculate_free_time(data, context):
                 # if its the first event, then the start time is right now
                 if doc_index == 0:
                     time_interval = {
-                        'start': datetime.now(timezone.utc),
-                        'end': interval_list[doc_Index]['start'],
+                        'start': datetime.now(datetime.timezone.utc),
+                        'end': group_cal_ref[doc_index]['start'],
                     }
                     freetime_list.append(time_interval)
                 # if is the last event, then the end time is 11:59 PM of that day
                 elif doc_index == (len(group_cal_ref) - 1):
                     time_interval = {
-                        'start': group_cal_ref[doc_Index]['end'],
-                        'end': group_cal_ref[doc_Index]['end'].replace(hour=11, minute=59),
+                        'start': group_cal_ref[doc_index]['end'],
+                        'end': group_cal_ref[doc_index]['end'].replace(hour=11, minute=59),
                     }
                     freetime_list.append(time_interval)
                 # If its in the middle calculations
                 else:
                     time_interval = {
-                        'start': group_cal_ref[doc_Index]['end'],
-                        'end': group_cal_ref[doc_Index + 1]['start']
+                        'start': group_cal_ref[doc_index]['end'],
+                        'end': group_cal_ref[doc_index + 1]['start']
                     }
                     freetime_list.append(time_interval)
             return freetime_list
