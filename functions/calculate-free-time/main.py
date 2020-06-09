e2ebf8c702ccad2d65e4d9f44d26244b3d64aff2
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-06-08 19:30:52 -0700

    Update main.py

--- functions/calculate-free-time/main.py
+++ functions/calculate-free-time/main.py
@@ -11,7 +11,35 @@
 from google.cloud import firestore
 from datetime import datetime
 
-client = firestore.client()
+client = firestore.Client()
+
+def find_free_time(busy_times):
+    freetime_list = list()
+    # freetime start = the end of the last event
+    # freetime end = the beginning of the next event
+    for doc_index in range(len(busy_times)):
+        # if its the first event, then the start time is right now
+        if doc_index == 0:
+            time_interval = {
+                'start': datetime.now(datetime.timezone.utc),
+                'end': busy_times[doc_index]['start'],
+            }
+            freetime_list.append(time_interval)
+        # if is the last event, then the end time is 11:59 PM of that day
+        elif doc_index == (len(busy_times) - 1):
+            time_interval = {
+                'start': busy_times[doc_index]['end'],
+                'end': busy_times[doc_index]['end'].replace(hour=11, minute=59),
+            }
+            freetime_list.append(time_interval)
+        # If its in the middle calculations
+        else:
+            time_interval = {
+                'start': busy_times[doc_index]['end'],
+                'end': busy_times[doc_index + 1]['start']
+            }
+            freetime_list.append(time_interval)
+    return freetime_list
 
 def calculate_free_time(data, context):
     ''' TRIGGERED by a change to the mergedCalendar (room calendar)
@@ -32,36 +60,13 @@ def calculate_free_time(data, context):
 
     @firestore.transactional
     def find_free_time(transaction, group_cal_ref):
-        freetime_list = list()
         group_cal_snapshot = group_cal_ref.get(transaction = transaction)
+        data = group_cal_snapshot.to_dict()
 
         if group_cal_snapshot.exists:
-            # freetime start = the end of the last event
-            # freetime end = the beginning of the next event
-            for doc_index in range(len(group_cal_ref)):
-                # if its the first event, then the start time is right now
-                if doc_index == 0:
-                    time_interval = {
-                        'start': datetime.now(datetime.timezone.utc),
-                        'end': group_cal_ref[doc_index]['start'],
-                    }
-                    freetime_list.append(time_interval)
-                # if is the last event, then the end time is 11:59 PM of that day
-                elif doc_index == (len(group_cal_ref) - 1):
-                    time_interval = {
-                        'start': group_cal_ref[doc_index]['end'],
-                        'end': group_cal_ref[doc_index]['end'].replace(hour=11, minute=59),
-                    }
-                    freetime_list.append(time_interval)
-                # If its in the middle calculations
-                else:
-                    time_interval = {
-                        'start': group_cal_ref[doc_index]['end'],
-                        'end': group_cal_ref[doc_index + 1]['start']
-                    }
-                    freetime_list.append(time_interval)
-            return freetime_list
+            freetime = find_free_time(data["calendar"])
+            client.document(u'rooms/{roomID}/entire-cal/freetime'.format(roomID = path_parts[1])).set({"calendar": freetime})
+            
 
-    freetime = find_free_time(transaction, group_cal_ref)
+    find_free_time(transaction, group_cal_ref)
 
-    client.document(u'rooms/{roomID}/entire-cal/freetime'.format(roomID = path_parts[1])).set({"calendar": freetime})
