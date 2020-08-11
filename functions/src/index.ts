70485dbb3a2756f7b283e66ed26ece606a30f4d9
Jason Jose <jasonsisonjose@csu.fullerton.edu>
2020-08-10 18:03:50 -0700

    added schedule cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from 'firebase-functions';
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 
+// FIREBASE DEPLOY:
+// firebase deploy -- only functions:testing
+
+
+// TODO:
+// For every single room, it checks the entire-cal/votes
+// and removes any voted time before Today()
 exports.testing = functions.pubsub
-  .schedule('every day 17:18')
+  .schedule('every day 18:02')
   .timeZone('America/Los_Angeles')
   .onRun(context => {
-
+    // admin.firestore().collection('rooms').get()
     console.log('triggered every 5 minutes', context)
   });
